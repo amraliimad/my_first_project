c5e439aaf4dda08e76a943361e0838635485f42d
amraliimad-gif <amraliimad@gmail.com>
2026-03-02 23:43:40 +0200

    Switch to PostgreSQL

--- malaeb_project/settings.py
+++ malaeb_project/settings.py
@@ -156,3 +156,17 @@ DATABASES = {
 # Heroku PostgreSQL integration
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True,
+    )
